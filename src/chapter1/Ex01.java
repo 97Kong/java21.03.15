3f2b189596522bde6c80dbab48e15838e162091a
DESKTOP-NUOPUIF\ITPS <ghrn15963@gmail.com>
2021-03-15 17:19:05 +0900

    최초 프로젝트 업로드

---
+++ src/chapter1/Ex01.java
@@ -0,0 +1,26 @@
+package chapter1;
+
+// 클래스 : 소스 코드의 최소 단위
+public class Ex01 {
+	// 메인 함수 : 시작점, 내가 작성한 소스코드의 시작이 어딘지 알려주는 기능
+	public static void main(String[] args) {
+		// 주석 : 메모, 개발자가 참고해야할 사항
+		// 한줄 지우기 = 컨트롤 + D
+		
+		// 무언가를 출력하기 위한 기능
+		// ( ) 안에다가 출력 대상을 지정
+		System.out.println("Hello World~!");	// Hello World 문자열 출력
+		System.out.println("주석 연습 중...");		// 주석 연습 중... 출력
+		System.out.println("출력1");
+		System.out.println("출력2");
+		
+		
+//		System.out.println("S출력2");	// 소드코드를 주석처리하면 실행되지 않음
+//		System.out.println();	// 컨트롤 + 시프트 + C  주석 만들기 -> 주석 지우기
+//		System.out.println();                                                                                                    
+		
+	} 
+}
+
+
+
---
+++ src/chapter1/Ex02.java
@@ -0,0 +1,37 @@
+package chapter1;
+
+public class Ex02 {
+
+	public static void main(String[] args) {
+		// 문자열 - 문자열 나열
+		// 뭉자열은 꼭 쌍따옴표를 사용해서 표현해야한다
+//		System.out.println("Hello World~!");
+		
+//		System.out.println("Hello");
+//		System.out.println("World~!");
+		
+//		System.out.print("I am ");
+//		System.out.print("a ");
+//		System.out.print("Java Programmer");
+		
+//		System.out.print("오늘은 ");
+//		System.out.print("자바 수업 ");
+//		System.out.print("첫 번째 날입니다.");
+		
+		// 정수
+//		System.out.println(1);
+//		// 실수
+//		System.out.println(3.14);
+//		// 문자
+//		System.out.println('a');
+//		//문자열
+//		System.out.println("abcd");
+		
+		System.out.println("오늘의 기온 : " + -5.3 + "ºC");
+		
+		System.out.println("커피 가격 : " + 1500);
+
+		System.out.print("철수의 java 과목 학점 : ");
+		System.out.print('A');
+	}
+}
